87dddcabb6afcea6f7732d61e13417c097ba6ae7
5G <james@giveback.ie>
2020-08-09 16:38:58 +0100

    add spotify.js

---
+++ src/spotify.js
@@ -0,0 +1,7 @@
+// https://developer.spotify/com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://192.168.1.9:3000/";
+
+const clientID = "7b4180ec9c3044d886ace64eb808a603";
