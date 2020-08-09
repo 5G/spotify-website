bf5a1695cec38cf69cffbba7efd69e7f42e7f758
5G <james@giveback.ie>
2020-08-09 16:35:31 +0100

    update login.js

--- src/Login.js
+++ src/Login.js
@@ -3,13 +3,10 @@ import "./Login.css";
 
 function Login() {
   return (
-    console.log(process.env.TEST),
-    (
-      <div className="login">
-        <img src="/spotify.jpg" alt="Spotify Logo" />
-        <a>LOGIN WITH SPOTIFY</a>
-      </div>
-    )
+    <div className="login">
+      <img src="/spotify.jpg" alt="Spotify Logo" />
+      <a>LOGIN WITH SPOTIFY</a>
+    </div>
   );
 }
 
