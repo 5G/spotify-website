0eec01dcbda2d13675e497efa84a6752618c15df
5G <james@giveback.ie>
2020-08-10 12:53:43 +0100

    Added DataLayer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 import Login from "./Login";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player";
+import { useDataLayerValue } from "./DataLayer";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{}, dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -27,16 +30,7 @@ function App() {
     console.log("TOKEN -> ", token);
   }, []);
 
-  return (
-    <div className="app">
-      {token ? (
-        //Render <Player />
-        <h1> I am logged in </h1>
-      ) : (
-        <Login />
-      )}
-    </div>
-  );
+  return <div className="app">{token ? <Player /> : <Login />}</div>;
 }
 
 export default App;
